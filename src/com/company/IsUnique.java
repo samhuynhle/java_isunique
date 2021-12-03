b8471b745257c5ce1d704b3e799f5f20e51e4e34
Sam Le <samhuynhle@gmail.com>
2021-12-03 02:01:26 -0800

    yes

--- src/com/company/IsUnique.java
+++ src/com/company/IsUnique.java
@@ -3,17 +3,16 @@ import java.util.HashSet;
 
 public class IsUnique {
     public static boolean isUnique(String str){
-        String[] str_arr = str.split("");
+        String[] arr = str.split("");
         HashSet<String> set = new HashSet<String>();
 
-        for(int i = 0; i < str_arr.length; i++){
-            if(set.contains(str_arr[i])) {
+        for(int i = 0; i < arr.length; i++){
+            if(set.contains(arr[i])) {
                 return false;
             } else {
-                set.add(str_arr[i]);
+                set.add(arr[i]);
             }
         }
-
         return true;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
@@ -6,5 +7,12 @@ public class Main {
         System.out.println("Determining Uniqueness");
         System.out.println("String is... " + (IsUnique.isUnique("String") ? "all unique" : "not unique"));
         System.out.println("Strrrrring is... " + (IsUnique.isUnique("Strrrrring") ? "all unique" : "not unique"));
+
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 1; i <= 5; i++){
+            System.out.print("Insert a word to check for unique characters: ");
+            String s = scanner.next();
+            System.out.println(s + " is... " + (IsUnique.isUnique(s) ? "all unique" : "not unique"));
+        }
     }
 }
